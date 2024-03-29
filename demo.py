2628a54435fa541bad8ea121e3589e94c6e9769c
sreevalsan02 <sreevalsans02@gmail.com>
2024-03-29 17:26:01 +0530

    made into a single function

--- demo.py
+++ demo.py
@@ -13,8 +13,22 @@ CORS(app)
 def table_image():
     if request.method =='POST':
         img_file = request.files['the_file']
+        model = RTDETR('best.pt')
+        img = Image.open(img_file)
+        results = model(img)
+        boxes = results[0].boxes
+        lis = []
+        for i in boxes.xyxy[0]:
+            lis.append(i.item())
+       
+
+        x = int(lis[0])
+        y = int(lis[1])
+        x_end = int(lis[2])
+        y_end = int(lis[3])
+
+        cropped_img = img.crop((x,y,x_end,y_end))
         
-        cropped_img = process_image(img_file)
         img_io = io.BytesIO()
         cropped_img.save(img_io, 'PNG')
         img_io.seek(0)
@@ -22,22 +36,4 @@ def table_image():
         
     return 'noo'
 
-def process_image(file):
-    model = RTDETR('best.pt')
-    img = Image.open(file)
-    results = model(img)
-    boxes = results[0].boxes
-    lis = []
-    for i in boxes.xyxy[0]:
-        lis.append(i.item())
-       
-
-    x = int(lis[0])
-    y = int(lis[1])
-    x_end = int(lis[2])
-    y_end = int(lis[3])
-
-    im1 = img.crop((x,y,x_end,y_end))
-    
-    return im1
 
