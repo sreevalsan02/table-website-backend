e0c0685bca8bddb1be7c595152e7c002655bb79a
Sreevalsan <98298661+sreevalsan02@users.noreply.github.com>
2024-04-05 14:28:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,38 +13,38 @@ CORS(app)
 def hello():
     return 'Hello, World!'
 
-# @app.route('/upload', methods=['GET', 'POST'])
+@app.route('/upload', methods=['GET', 'POST'])
 
-# def table_image():
-#     if request.method =='POST':
-#         img_file = request.files['the_file']
+def table_image():
+    if request.method =='POST':
+        img_file = request.files['the_file']
         
-#         cropped_img = process_image(img_file)
-#         img_io = io.BytesIO()
-#         cropped_img.save(img_io, 'PNG')
-#         img_io.seek(0)
-#         return send_file(img_io, mimetype='image/png')
+        cropped_img = process_image(img_file)
+        img_io = io.BytesIO()
+        cropped_img.save(img_io, 'PNG')
+        img_io.seek(0)
+        return send_file(img_io, mimetype='image/png')
         
-#     return 'noo'
+    return 'noo'
 
-# def process_image(file):
-#     model = RTDETR('best.pt')
-#     img = Image.open(file)
-#     results = model(img)
-#     boxes = results[0].boxes
-#     lis = []
-#     for i in boxes.xyxy[0]:
-#         lis.append(i.item())
+def process_image(file):
+    model = RTDETR('best.pt')
+    img = Image.open(file)
+    results = model(img)
+    boxes = results[0].boxes
+    lis = []
+    for i in boxes.xyxy[0]:
+        lis.append(i.item())
        
 
-#     x = int(lis[0])
-#     y = int(lis[1])
-#     x_end = int(lis[2])
-#     y_end = int(lis[3])
+    x = int(lis[0])
+    y = int(lis[1])
+    x_end = int(lis[2])
+    y_end = int(lis[3])
 
-#     im1 = img.crop((x,y,x_end,y_end))
+    im1 = img.crop((x,y,x_end,y_end))
     
-#     return im1
+    return im1
 
 if __name__ =="__main__":
     app.run()
